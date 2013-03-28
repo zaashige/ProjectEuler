6ddb7727b038e09f4a9f5453ce1667943f685c40
zaashige <zaashige@gmail.com>
2013-03-29 08:58:12 +1030

    Solves Problem #7

---
+++ problem 7.rb
@@ -0,0 +1,20 @@
+=begin
+
+Project Euler - Problem #7
+
+	By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+	What is the 10 001st prime number?
+
+=end
+
+require 'mathn'
+require 'benchmark'
+time = Benchmark.realtime do
+
+# Begin Solution
+	puts((Prime.first 10_001).last)
+# End Solution
+
+end
+puts("Solved in #{time} seconds.")
